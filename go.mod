004343b78b0ab6f7031b81591d83ef88ee6dcdbb
Marek Michalik <marekmichalik43@gmail.com>
2024-08-23 01:21:20 +0200

    Rewrite to TCP/IP

--- go.mod
+++ go.mod
@@ -1,13 +1,3 @@
 module github.com/marekm4/database
 
-go 1.20
-
-require (
-	github.com/gorilla/websocket v1.5.1
-	gotest.tools/v3 v3.5.0
-)
-
-require (
-	github.com/google/go-cmp v0.5.9 // indirect
-	golang.org/x/net v0.19.0 // indirect
-)
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+func main() {
+	delim := byte('\n')
+	database := NewDatabase()
+
+	filename := "database"
+	if len(os.Getenv("FILE")) > 0 {
+		filename = os.Getenv("FILE")
+	}
+
+	err := Load(database, filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-signals
+		err := Dump(database, filename)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		os.Exit(0)
+	}()
+
+	port := "8080"
+	if len(os.Getenv("PORT")) > 0 {
+		port = os.Getenv("PORT")
+	}
+
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		go func(net.Conn, Database) {
+			reader := bufio.NewReader(connection)
+			writer := bufio.NewWriter(connection)
+
+			for {
+				message, err := reader.ReadString(delim)
+				if err != nil {
+					log.Println(err)
+					break
+				}
+
+				query := ParseQuery(strings.TrimSuffix(message, string(delim)))
+				values := query.Execute(database)
+
+				_, err = writer.WriteString(strings.Join(values, "\n") + string(delim))
+				if err != nil {
+					log.Println(err)
+					break
+				}
+				err = writer.Flush()
+				if err != nil {
+					log.Println(err)
+					break
+				}
+			}
+			err = connection.Close()
+			if err != nil {
+				log.Println(err)
+			}
+		}(connection, database)
+	}
+
+	err = listener.Close()
+	if err != nil {
+		log.Println(err)
+	}
+}
